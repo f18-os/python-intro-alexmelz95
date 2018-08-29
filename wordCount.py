ffd98ae8670ee059cf58e254582c5cd3eac5ad54
DESKTOP-S7PMGJS\alexm <ammelendez4@miners.utep.edu>
2018-08-28 23:17:38 -0600

    Removed unnecessary imports from wordCount.py

--- wordCount.py
+++ wordCount.py
@@ -1,7 +1,6 @@
 import sys        # command line arguments
 import re         # regular expression tools
 import os         # checking if file exists
-import subprocess # executing program
 
 regex = re.compile('[^a-zA-Z]')
 
