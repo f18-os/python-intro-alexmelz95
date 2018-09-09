cbf418e122a5f49a3588e0f38a95b54cb340d4fa
DESKTOP-S7PMGJS\alexm <ammelendez4@miners.utep.edu>
2018-09-09 17:30:31 -0600

    added exec

--- shellLab/shell.py
+++ shellLab/shell.py
@@ -18,13 +18,15 @@ elif rc == 0:                   # child
         command = raw_input("prompt>")
         args = command.split();
 
-    for dir in re.split(":", os.environ['PATH']): # try each directory in the path
-        program = "%s/%s" % (dir, args[0])
-        os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
-        try:
-            os.execve(program, args, os.environ) # try to exec program
-        except FileNotFoundError:             # ...expected
-            pass                              # ...fail quietly
+    # for dir in re.split(":", os.environ['PATH']): # try each directory in the path
+    #     program = "%s/%s" % (dir, args[0])
+    #     os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
+    #     try:
+    #         os.execve(program, args, os.environ) # try to exec program
+    #     except FileNotFoundError:             # ...expected
+    #         pass                              # ...fail quietly
+
+    print(args)
 
     os.write(2, ("Child:    Could not exec %s\n" % args[0]).encode())
     sys.exit(1)
