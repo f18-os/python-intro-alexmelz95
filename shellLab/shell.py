c3a3ffaa18d82447c08f215a3119aee47e67e888
DESKTOP-S7PMGJS\alexm <ammelendez4@miners.utep.edu>
2018-09-09 19:01:19 -0600

    added killswitch

--- shellLab/shell.py
+++ shellLab/shell.py
@@ -13,33 +13,37 @@ if rc < 0:
     sys.exit(1)
 elif rc == 0:                   # child
     os.write(1, ("I am child.  My pid==%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
-    args = []
-    while len(args) == 0:
-        command = input("prompt> ")
-        args = command.split();
+    exit = false
+    while(exit):
+        args = []
+        while len(args) == 0:
+            command = input("prompt> ")
+            args = command.split()
 
-    outputFile = ""
-    for i in range(len(args)):
-        if args[i] == ">":
-            outputFile = args[i+1]
-            os.close(1)                 # redirect child's stdout
-            sys.stdout = open(outputFile, "w+")
-            fd = sys.stdout.fileno() # os.open("p4-output.txt", os.O_CREAT)
-            os.set_inheritable(fd, True)
-            os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
-            args2 = args[:i]
+        if args[0] == "kill"
+            sys.exit(0)
 
-    for dir in re.split(":", os.environ['PATH']): # try each directory in the path
-        program = "%s/%s" % (dir, args2[0])
-        os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
-        try:
-            os.execve(program, args2, os.environ) # try to exec program
-        except FileNotFoundError:             # ...expected
-            pass                              # ...fail quietly
+        outputFile = ""
+        for i in range(len(args)):
+            if args[i] == ">":
+                outputFile = args[i+1]
+                os.close(1)                 # redirect child's stdout
+                sys.stdout = open(outputFile, "w+")
+                fd = sys.stdout.fileno() # os.open("p4-output.txt", os.O_CREAT)
+                os.set_inheritable(fd, True)
+                os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
+                args2 = args[:i]
+
+        for dir in re.split(":", os.environ['PATH']): # try each directory in the path
+            program = "%s/%s" % (dir, args2[0])
+            os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
+            try:
+                os.execve(program, args2, os.environ) # try to exec program
+            except FileNotFoundError:             # ...expected
+                pass                              # ...fail quietly
 
     os.write(2, ("Child:    Could not exec %s\n" % args[0]).encode())
     sys.exit(1)
-    # os.write(1, (command[0] + "\n").encode())
 else:                           # parent (forked ok)
     os.write(1, ("I am parent.  My pid=%d.  Child's pid=%d\n" % (pid, rc)).encode())
     childPidCode = os.wait()
